8a5ace77fbf0b0a8ab63b0e121582c04e2e744bc
Kevin Do <kevindokhoale@gmail.com>
2016-11-14 22:08:52 -0800

    React
    [+] JSX Prop passing

--- reactSite/src/www/js/site.jsx
+++ reactSite/src/www/js/site.jsx
@@ -1,1 +1,36 @@
 "use strict";
+const 
+    React = require("react"), 
+    ReactDOM =require("react-dom");
+
+let 
+    ItemList = React.createClass({
+
+        render: function(){
+            //this represents the component
+            // props refers to the passed in parameters
+            
+            let items = this.props.items.map(function(item){
+                return <li>{item}</li>
+            });
+
+            return <div>
+             <h1> {this.props.header} </h1>
+
+                <ul>
+                {items}
+                </ul>
+            
+            </div>
+            ;
+        }
+         
+    });
+
+let
+    items = [
+        "one", "two", "doot didily"
+    ];
+
+ReactDOM.render(<ItemList header='ItemList' items = {items}/>, 
+    document.querySelector("[class = 'container']"));
