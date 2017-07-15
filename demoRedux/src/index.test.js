fd2a2418829fe8c86b7919bb38d1b0639ccf9c10
Kevin Do <kevindokhoale@gmail.com>
2017-07-15 15:14:43 -0700

    Initial Demo Commit
    [+] Setup dependencies
    [+] Setup babel for bundling/minify
    [+] Setup demo test for mocha
    [+] Setup hot reloading and lint:watch
    [+] npm scripts to run all in parallel

---
+++ demoRedux/src/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Our first test', () => {
+    it('hi lets pass', () => {
+        expect(true).toEqual(true);
+    });
+});
---
+++ demoRedux/tools/startMessage.js
@@ -0,0 +1,3 @@
+import colors from 'colors';
+
+console.log('starting up app in dev mode'.green);
